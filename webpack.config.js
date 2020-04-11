0a3ff22892eb6e0f73f5b20f72d0423b132e9d25
Brian Morearty <brian@morearty.org>
2020-04-11 09:16:27 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+
+  entry: {
+    app: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+  },
+
+  plugins: [
+    new CopyWebpackPlugin(
+      [
+        {
+          from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'build'),
+        }
+      ]
+    )
+  ]
+};
